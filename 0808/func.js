d339c5819d60589ab725506a8c994844c04b6204
yoonsejong <oxwk123@gmail.com>
2022-08-08 16:36:18 +0900

    자바스크립트 함수 공부시작

---
+++ 0808/func.js
@@ -0,0 +1,16 @@
+// 함수 선언문
+function add(x,y) {
+    return x + y;
+}
+
+// 함수 표현식
+
+let add2 = function (x, y) {
+    return x + y;
+};
+
+// Function 생성자 함수
+let add = new Function('x', 'y', 'return x+y');
+
+//화살표 함수
+let add = (x, y) => x + y;
---
+++ 0808/index.js
@@ -0,0 +1,12 @@
+let person1 ={
+    name: 'yoon'
+}
+
+let person2 = {
+    name: 'yoon'
+}
+
+
+console.log(person1 === person2); // false
+
+console.log(person1.name === person2.name); // true
