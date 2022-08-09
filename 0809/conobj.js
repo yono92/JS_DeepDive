f06185300087e777ed89d584b8f5326fe1fe2569
yoonsejong <oxwk123@gmail.com>
2022-08-09 16:48:52 +0900

    함수 파트 마무리

---
+++ 0809/conobj.js
@@ -0,0 +1,13 @@
+/**
+ * const 키워드로 선언된 변수에 객체를 할당한 경우 값을 변경할 수 있다.
+ */
+
+const person = {
+    name: 'yoonsejong'
+};
+
+person.name = 'yoon';
+
+console.log(person);
+
+// const 키워드는 재할당을 금지할 뿐 "불변"을 의미하지는 않는다. 
